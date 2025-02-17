150f87d9c0de3761148f6510aea5e39370de2839
Berke Öz <beerkeozz@outlook.com>
2025-02-17 17:45:35 +0300

    test

---
+++ src/components/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+const SignUpPage = () => {
+    return (
+        <Layout>
+            <ShopCards />
+            <FilterShopPage />
+            <ShopProductCard />
+            <Clients />
+
+        </Layout>
+    );
+};
+
+export default SignUpPage;
