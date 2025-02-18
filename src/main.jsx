55a8a7209b253e397861b09437d06d85eeef03ee
Berke Öz <beerkeozz@outlook.com>
2025-02-19 01:12:05 +0300

    syntax hataları fixlendi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const store = configureStore();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  <App />
-
-)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
+import { thunk } from 'redux-thunk';
 
 const configureStore = () => {
     const store = createStore(
